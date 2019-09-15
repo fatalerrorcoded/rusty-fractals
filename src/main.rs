d875f3fe007719e8eb53acc0243600b9fd10a085
segfault__ <segfault__@localhost.localdomain>
2019-09-15 17:48:30 +0200

    GL and winit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate gl;
+
+use winit::{Event, WindowEvent};
+
 fn main() {
+    let mut events_loop = winit::EventsLoop::new();
+    let window = winit::WindowBuilder::new().with_title("Fractals").build(&events_loop).unwrap();
     println!("Hello, world!");
+
+    let mut running = true;
+    while running {
+        events_loop.poll_events(|event| {
+            match event {
+                Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => running = false,
+                _ => ()
+            };
+        });
+    }
 }
