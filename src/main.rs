a38cdee01861d95626ef12df6a880fb920219b4b
segfault__ <segfault__@localhost.localdomain>
2019-09-16 05:43:27 +0200

    SDL2 instead of winit couldn't figure out how to gl load with winit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
-extern crate gl;
-
-use winit::{Event, WindowEvent};
+use sdl2::event::Event;
 
 fn main() {
-    let mut events_loop = winit::EventsLoop::new();
-    let window = winit::WindowBuilder::new().with_title("Fractals").build(&events_loop).unwrap();
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Fractals", 800, 600)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
     println!("Hello, world!");
 
-    let mut running = true;
-    while running {
-        events_loop.poll_events(|event| {
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
             match event {
-                Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => running = false,
+                Event::Quit {..} => break 'main,
                 _ => ()
             };
-        });
+        }
+
+        unsafe {
+            gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
     }
 }
